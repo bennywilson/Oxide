5e2358c7788b2598209620d68ef2e6b6d45d8694
Benjamin Wilson <bennywilson@benny-wilson.com>
2021-04-03 17:21:15 -0400

    Stubbing in gas and steering

--- Assets/Prototype/Scripts/GameController.cs
+++ Assets/Prototype/Scripts/GameController.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameController : MonoBehaviour
 {
+    Vehicle PlayerVehicle;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerVehicle = FindObjectOfType<Vehicle>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void OnMove(InputValue input)
+    {
+        Debug.Log("Here!");
+        if (PlayerVehicle == null)
+        {
+            return;
+        }
+
+        PlayerVehicle.OnMoveInput(input);
+
+    }
+
+    public void OnGas(InputValue input)
+    {
+        Debug.Log("Woah!");
+
+        if (PlayerVehicle == null)
+        {
+            return;
+        }
+
+        PlayerVehicle.OnGas(input);
+
+        Debug.Log(Time.time + " : " + input.Get<float>());
     }
 }
--- Assets/Prototype/Scripts/Vehicle.cs
+++ Assets/Prototype/Scripts/Vehicle.cs
@@ -1,10 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Vehicle : MonoBehaviour
 {
     Rigidbody RB;
+    Vector2 SteeringVector;
+    float Gas;
+
+    [SerializeField]
+    float TurnRate;
+
+    [SerializeField]
+    float MaxSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +22,24 @@ public class Vehicle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        RB.velocity = new Vector3(0.0f, 0.0f, 4.0f);
+        RB.velocity = new Vector3(0.0f, 0.0f, 4.0f * Gas);
+    }
+
+    private void LateUpdate()
+    {
+
+        this.transform.Rotate(0.0f, SteeringVector.x * TurnRate, 0.0f, Space.World);
+    }
+
+    public void OnMoveInput(InputValue Value)
+    {
+        SteeringVector = Value.Get<Vector2>();
+
+        Debug.Log(" -> " + SteeringVector);//
+    }
+
+    public void OnGas(InputValue Value)
+    {
+        Gas = Value.Get<float>();
     }
 }
