f6848bf1ecef0fd35b53914b1aeeda5780af2aa3
Benjamin Wilson <bennywilson@benny-wilson.com>
2021-04-04 08:40:03 -0400

    Movement and steering WIP

--- Assets/Prototype/Scripts/Vehicle.cs
+++ Assets/Prototype/Scripts/Vehicle.cs
@@ -13,35 +13,52 @@ public class Vehicle : MonoBehaviour
     [SerializeField]
     float MaxSpeed;
 
+    [SerializeField]
+    float Acceleration = 1.0f;
+
+    [SerializeField]
+    float Friction = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
+        RB = gameObject.AddComponent(typeof(Rigidbody)) as Rigidbody;
         RB = GetComponentInChildren<Rigidbody>();
+        RB.hideFlags = HideFlags.NotEditable;
+    }
+
+    private void Update()
+    {
+        RB.drag = Friction;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Vector3 vel = transform.forward;
         vel *= MaxSpeed * Gas;
-        RB.velocity = vel;
+        RB.velocity += transform.forward * Gas * Acceleration;
+
+        if (Mathf.Abs(SteeringVector.x) > 0.1f)
+        {
+            transform.rotation *= Quaternion.AngleAxis(SteeringVector.x, transform.up);
+        }
     }
 
     private void LateUpdate()
     {
 
-        this.transform.Rotate(0.0f, SteeringVector.x * TurnRate, 0.0f, Space.World);
+       // this.transform.Rotate(0.0f, SteeringVector.x * TurnRate, 0.0f, Space.World);
     }
 
     public void OnMoveInput(InputValue Value)
     {
         SteeringVector = Value.Get<Vector2>();
-
-        Debug.Log(" -> " + SteeringVector);//
     }
 
     public void OnGas(InputValue Value)
     {
+    //    RB.lin
         Gas = Value.Get<float>();
     }
 }
