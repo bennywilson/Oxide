8beac13c7e7d6b692212f984127143a1c397b340
Evan Reidland <er@evanreidland.com>
2021-04-11 13:35:17 -0500

    Oxide -> OxideInput, also generating C# class and polling it for input so we can get values at 0.

--- Assets/Prototype/Scripts/GameController.cs
+++ Assets/Prototype/Scripts/GameController.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 public class GameController : MonoBehaviour
 {
     VehicleBase PlayerVehicle;
+    OxideInput _oxideInput;
 
     VehicleInput GetInput()
     {
@@ -28,39 +29,51 @@ public class GameController : MonoBehaviour
         return PlayerVehicle != null && PlayerVehicle.gameObject.activeInHierarchy;
     }
 
+    void Awake()
+    {
+        _oxideInput = new OxideInput();
+
+        _oxideInput.Player.Prrr.performed += c =>
+        {
+            if (!GetCanUseInput())
+                return;
+
+            var vInput = GetInput();
+            vInput.WantsToPurr = true;
+            SetInput(vInput);
+        };
+    }
+
+    void OnEnable()
+    {
+        _oxideInput.Enable();
+    }
+
+    void OnDisable()
+    {
+        if (_oxideInput != null)
+        {
+            _oxideInput.Disable();
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         PlayerVehicle = FindObjectOfType<VehicleBase>();
     }
 
-    public void OnMove(InputValue input)
+    void Update()
     {
         if (!GetCanUseInput())
             return;
 
+        var playerInput = _oxideInput.Player;
         var vInput = GetInput();
-        vInput.Steering = input.Get<Vector2>().x;
-        SetInput(vInput);
-    }
 
-    public void OnGas(InputValue input)
-    {
-        if (!GetCanUseInput())
-            return;
-
-        var vInput = GetInput();
-        vInput.Gas = input.Get<float>();
-        SetInput(vInput);
-    }
-
-    public void OnPrrr(InputValue Input)
-    {
-        if (!GetCanUseInput())
-            return;
-
-        var vInput = GetInput();
-        vInput.WantsToPurr = true;
+        vInput.Steering = playerInput.Move.ReadValue<Vector2>().x;
+        vInput.Gas = playerInput.Gas.ReadValue<float>();
+        
         SetInput(vInput);
     }
 }
